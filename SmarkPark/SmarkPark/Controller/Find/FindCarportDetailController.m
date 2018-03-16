57b16ee1f5a025a0a998492cef5712b7153731e2
GRIM <694130310@qq.com>
2018-03-16 15:03:01 +0800

    no message

--- SmarkPark/SmarkPark/Controller/Find/FindCarportDetailController.m
+++ SmarkPark/SmarkPark/Controller/Find/FindCarportDetailController.m
@@ -12,8 +12,11 @@
 #import <AMapFoundationKit/AMapFoundationKit.h>
 #import <MAMapKit/MAMapKit.h>
 #import "MinePointAnnotation.h"
+#import <AMapSearchKit/AMapSearchKit.h>
+#import "GeocodeAnnotation.h"
+#import <AMapNaviKit/AMapNaviKit.h>
 
-@interface FindCarportDetailController ()<AMapLocationManagerDelegate, MAMapViewDelegate>
+@interface FindCarportDetailController ()<AMapLocationManagerDelegate, MAMapViewDelegate, AMapSearchDelegate, AMapNaviDriveManagerDelegate>
 
 //地图
 @property (weak, nonatomic) IBOutlet UIView *mapView;
@@ -25,13 +28,20 @@
 @property (weak, nonatomic) IBOutlet UILabel *planTime;
 //确认预定
 @property (weak, nonatomic) IBOutlet UIButton *sureBtn;
-//用户位置标注
-@property(nonatomic, strong)MAPinAnnotationView *minePinView;
-@property(nonatomic, strong)MinePointAnnotation *minePoint;
 
 //地图相关
 @property(nonatomic, strong)MAMapView *aMapView;
 @property(nonatomic, strong)AMapLocationManager *locationManager;
+//用户位置标注
+@property(nonatomic, strong)MAPinAnnotationView *minePinView;
+@property(nonatomic, strong)MinePointAnnotation *minePoint;
+@property(nonatomic, strong)AMapSearchAPI *search;
+/* 起始点经纬度. */
+@property (nonatomic) CLLocationCoordinate2D startCoordinate;
+/* 终点经纬度. */
+@property (nonatomic) CLLocationCoordinate2D destinationCoordinate;
+
+
 
 @end
 
@@ -72,6 +82,7 @@
     _aMapView.showsUserLocation = true;
     _aMapView.delegate = self;
     
+    //定位
     self.locationManager = [[AMapLocationManager alloc] init];
     self.locationManager.delegate = self;
     [self.locationManager setLocatingWithReGeocode:YES];
@@ -80,6 +91,93 @@
     if ([AMapLocationManager headingAvailable]) {
         [self.locationManager startUpdatingHeading];
     }
+    
+    //搜索
+    self.search = [[AMapSearchAPI alloc] init];
+    self.search.delegate = self;
+    AMapGeocodeSearchRequest *geo = [[AMapGeocodeSearchRequest alloc] init];
+    geo.address = @"四川大学望江校区";
+    [self.search AMapGeocodeSearch:geo];
+
+    
+    //导航
+    [[AMapNaviDriveManager sharedInstance] setDelegate:self];
+    
+}
+
+#pragma mark - 展示路线方案
+- (void)showRouteNavi{
+
+    AMapNaviPoint *start = [AMapNaviPoint locationWithLatitude:_startCoordinate.latitude longitude:_startCoordinate.longitude];
+    AMapNaviPoint *end = [AMapNaviPoint locationWithLatitude:_destinationCoordinate.latitude longitude:_destinationCoordinate.longitude];
+    [[AMapNaviDriveManager sharedInstance] calculateDriveRouteWithStartPoints:@[start] endPoints:@[end] wayPoints:nil drivingStrategy:AMapNaviDrivingStrategyMultipleDefault];
+}
+
+- (void)driveManagerOnCalculateRouteSuccess:(AMapNaviDriveManager *)driveManager{
+    NSLog(@"onCalculateRouteSuccess");
+    
+    //显示路径或开启导航
+    NSUInteger count = driveManager.naviRoute.routeCoordinates.count;
+    __block CLLocationCoordinate2D *coords = malloc(count * sizeof(CLLocationCoordinate2D));
+    [driveManager.naviRoute.routeCoordinates enumerateObjectsUsingBlock:^(AMapNaviPoint * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        coords[idx] = CLLocationCoordinate2DMake(obj.latitude, obj.longitude);
+    }];
+    MAPolyline *polyline = [MAPolyline polylineWithCoordinates:coords count:count];
+    [_aMapView removeOverlays:_aMapView.overlays];
+    [_aMapView addOverlay:polyline];
+    
+    NSInteger hour = driveManager.naviRoute.routeTime / 3600;
+    NSInteger second = driveManager.naviRoute.routeTime / 60;
+    
+    [AlertView showMsg:[NSString stringWithFormat:@"%ld小时%ld分钟", hour, second] duration:1.5];
+}
+
+- (void)driveManager:(AMapNaviDriveManager *)driveManager onCalculateRouteFailure:(NSError *)error{
+    NSLog(@"路线规划失败 %@", error);
+}
+
+- (MAOverlayRenderer *)mapView:(MAMapView *)mapView rendererForOverlay:(id<MAOverlay>)overlay{
+    if ([overlay isKindOfClass:[MAPolyline class]]) {
+        mapView.visibleMapRect = overlay.boundingMapRect;
+        
+        MAPolylineRenderer *render = [[MAPolylineRenderer alloc]initWithOverlay:overlay];
+        render.lineWidth = 8;
+        render.strokeColor = [UIColor greenColor];
+
+        return render;
+    }
+    return nil;
+}
+
+
+#pragma mark - AMapSearchDelegate
+- (void)onGeocodeSearchDone:(AMapGeocodeSearchRequest *)request response:(AMapGeocodeSearchResponse *)response{
+    if (response.geocodes.count == 0){
+        return;
+    }
+    
+    NSMutableArray *annotations = [NSMutableArray array];
+    [response.geocodes enumerateObjectsUsingBlock:^(AMapGeocode *obj, NSUInteger idx, BOOL *stop) {
+        GeocodeAnnotation *geocodeAnnotation = [[GeocodeAnnotation alloc] initWithGeocode:obj];
+        [annotations addObject:geocodeAnnotation];
+    }];
+    
+    if (annotations.count == 1){
+        [self.aMapView setCenterCoordinate:[(GeocodeAnnotation *)annotations[0] coordinate] animated:YES];
+        _destinationCoordinate = [(GeocodeAnnotation *)annotations[0] coordinate];
+        //路线规划
+        [self showRouteNavi];
+    }else{
+        [self.aMapView showAnnotations:annotations animated:YES];
+        [AlertView showMsg:@"目标停车位地址不够具体明确，存在多个位置" duration:1.5];
+    }
+    
+    [self.aMapView addAnnotations:annotations];
+    
+}
+
+- (void)AMapSearchRequest:(id)request didFailWithError:(NSError *)error{
+    NSLog(@"搜索停车地点失败:Error: %@", error);
 }
 
 
@@ -89,11 +187,10 @@
     if (_isFirstLoad) {
         _isFirstLoad = false;
         _minePoint = [[MinePointAnnotation alloc]init];
-        _minePoint.coordinate = _aMapView.centerCoordinate;
-        _minePoint.lockedScreenPoint = CGPointMake(SCREEN_WIDTH * 0.5, SCREEN_WIDTH * 0.5);
+        _minePoint.coordinate = location.coordinate;
+        _startCoordinate = location.coordinate;
         [_aMapView addAnnotation:_minePoint];
         [_aMapView showAnnotations:@[_minePoint] animated:true];
-        [_aMapView setCenterCoordinate:location.coordinate animated:true];
     }
     
     NSLog(@"location:{lat:%f; lon:%f; accuracy:%f}", location.coordinate.latitude, location.coordinate.longitude, location.horizontalAccuracy);
@@ -121,7 +218,17 @@
         _minePinView = pinView;
         return pinView;
     }
-    return nil;
+    
+    //目的地位置
+    static NSString *pinID = @"destination";
+    MAPinAnnotationView *pinView = (MAPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:pinID];
+    if (pinView == nil) {
+        pinView = [[MAPinAnnotationView alloc]initWithAnnotation:annotation reuseIdentifier:pinID];
+    }
+    pinView.image = [UIImage imageNamed:@"map_carport"];
+    pinView.animatesDrop = true;
+    pinView.canShowCallout = true;
+    return pinView;
 }
 
 #pragma mark - 确认预定
@@ -133,6 +240,10 @@
 - (void)dealloc{
     _locationManager.delegate = nil;
     _aMapView.delegate = nil;
+    [[AMapNaviDriveManager sharedInstance] stopNavi];
+    [[AMapNaviDriveManager sharedInstance] setDelegate:nil];
+    BOOL success = [AMapNaviDriveManager destroyInstance];
 }
+
 
 @end
